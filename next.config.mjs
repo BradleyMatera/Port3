ebf0c7621322e2cced75daa8ee9de706885510d2
MateraBradley-FS-1 <BFMATERA@student.fullsail.edu>
2025-01-09 20:05:06 -0600

    add Spotify environment variables to Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
